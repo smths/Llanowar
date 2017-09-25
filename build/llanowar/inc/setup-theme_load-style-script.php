1e0d290626ed967e0f3e063f517571698f9d7bc0
smths <sho.matsuhashi@gmail.com>
2017-09-26 07:30:12 +0900

    サニタイズcss追加

--- build/llanowar/inc/setup-theme_load-style-script.php
+++ build/llanowar/inc/setup-theme_load-style-script.php
@@ -4,8 +4,11 @@ function init_scripts() {
     /* テンプレートcss読み込み */
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
+	/*  sanitize.css読み込み */
+    wp_enqueue_style( 'sanitize_css', get_stylesheet_directory_uri() . '/dll/sanitize_css/sanitize.css.css',array('style') );
+
     /* 共通css読み込み */
-    wp_enqueue_style( 'common-style', get_stylesheet_directory_uri() . '/css/common-style.css' );
+    wp_enqueue_style( 'common-style', get_stylesheet_directory_uri() . '/css/common-style.css',array('style','sanitize_css') );
 
     /* jquery読み込み */
     wp_enqueue_script( 'jquery' );
