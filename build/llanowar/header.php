fad8d90f4203458e13342cb0adc55becb04fe33c
smths <sho.matsuhashi@gmail.com>
2017-10-08 15:09:26 +0900

    ヘッダナビをドロップダウンに変更

--- build/llanowar/header.php
+++ build/llanowar/header.php
@@ -15,11 +15,54 @@
 			<div class="site-header_content__btn"><span class="c-table"><span class="c-table__midle"><label class="header_btn" for="sidebar_flag"><span class="c-hamburger"><span class="c-hamburger__bar"></span><span class="c-hamburger__bar"></span><span class="c-hamburger__bar"></span></span></label></span></span></div>
 			<div class="site-header_content__nav">
 				<ul>
-					<li><a class="c-hover_action" href=""><span class="c-table"><span class="c-table__midle"><span class="header_nav">aaaaa</span></span></span></a></li>
-					<li><a class="c-hover_action" href=""><span class="c-table"><span class="c-table__midle"><span class="header_nav">aaaaa</span></span></span></a></li>
-					<li><a class="c-hover_action" href=""><span class="c-table"><span class="c-table__midle"><span class="header_nav">aaaaa</span></span></span></a></li>
-					<li><a class="c-hover_action" href=""><span class="c-table"><span class="c-table__midle"><span class="header_nav">aaaaa</span></span></span></a></li>
-					<li><a class="c-hover_action" href=""><span class="c-table"><span class="c-table__midle"><span class="header_nav">aaaaa</span></span></span></a></li>
+					<li>
+						<p class="site-header_content__nav_title"><span class="c-hover_action u-height_100"><span class="c-table"><span class="c-table__midle"><span class="header_nav_title">あああああ</span></span></span></span></p>
+						<ul class="site-header_nav_inner_list">
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="site-header_content__nav_title"><span class="c-hover_action u-height_100"><span class="c-table"><span class="c-table__midle"><span class="header_nav_title">あああああ</span></span></span></span></p>
+						<ul class="site-header_nav_inner_list">
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="site-header_content__nav_title"><span class="c-hover_action u-height_100"><span class="c-table"><span class="c-table__midle"><span class="header_nav_title">あああああ</span></span></span></span></p>
+						<ul class="site-header_nav_inner_list">
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="site-header_content__nav_title"><span class="c-hover_action u-height_100"><span class="c-table"><span class="c-table__midle"><span class="header_nav_title">あああああ</span></span></span></span></p>
+						<ul class="site-header_nav_inner_list">
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+						</ul>
+					</li>
+					<li>
+						<p class="site-header_content__nav_title"><span class="c-hover_action u-height_100"><span class="c-table"><span class="c-table__midle"><span class="header_nav_title">あああああ</span></span></span></span></p>
+						<ul class="site-header_nav_inner_list">
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+							<li><a class="c-hover_action u-height_100" href=""><span class="c-table" style="width:100%;"><span class="c-table__midle" style="width:100%;"><span class="header_nav_item">ああああああ</span></span></span></a></li>
+						</ul>
+					</li>
 				</ul>
 			</div>
 		</div>
