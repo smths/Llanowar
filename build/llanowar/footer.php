aa78b1569c943514b56ff1b4bb76f889e7d7a050
smths <sho.matsuhashi@gmail.com>
2017-10-11 00:29:32 +0900

    フッターのベース追加

--- build/llanowar/footer.php
+++ build/llanowar/footer.php
@@ -1,4 +1,21 @@
 <footer id="site-footer" class="site-footer">
+    <div class="site-footer__inner">
+        <div class="site-footer_logo">
+
+        </div>
+        <div class="site-footer_info">
+
+        </div>
+        <div class="site-footer_nav">
+
+        </div>
+        <div class="site-footer_bottom_info">
+
+        </div>
+        <div class="site-footer_copyright">
+
+        </div>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
